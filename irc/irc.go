ebfe14308813a23daceb0454279e6224fe926382
Jeremiah Orem <oremj@oremj.com>
2014-07-15 17:46:27 -0700

    irc: ParsePrefix, DialTLS, s/Connection/Conn/

--- irc/irc.go
+++ irc/irc.go
@@ -2,6 +2,7 @@ package irc
 
 import (
 	"bufio"
+	"crypto/tls"
 	"io"
 	"net"
 	"strings"
@@ -13,14 +14,31 @@ import (
 type Message struct {
 	Command string
 	Params  []string
-	Prefix  string
+	Prefix  *Prefix
 	Raw     string
-	User    string
 	Txt     string
 }
 
+type Prefix struct {
+	Name string
+	User string
+	Host string
+}
+
 // Parses an incoming IRC message in the format:
-//   [:Prefix] Command [ { Param } ] [:Txt]
+//    message    =  [ ":" prefix SPACE ] command [ params ] crlf
+//    prefix     =  servername / ( nickname [ [ "!" user ] "@" host ] )
+//    command    =  1*letter / 3digit
+//    params     =  *14( SPACE middle ) [ SPACE ":" trailing ]
+//               =/ 14( SPACE middle ) [ SPACE [ ":" ] trailing ]
+//
+//    nospcrlfcl =  %x01-09 / %x0B-0C / %x0E-1F / %x21-39 / %x3B-FF
+//                    ; any octet except NUL, CR, LF, " " and ":"
+//    middle     =  nospcrlfcl *( ":" / nospcrlfcl )
+//    trailing   =  *( ":" / " " / nospcrlfcl )
+//
+//    SPACE      =  %x20        ; space character
+//    crlf       =  %x0D %x0A   ; "carriage return" "linefeed"
 func ParseMessage(l string) *Message {
 	split2 := func(s, sep string) (string, string) {
 		parts := strings.SplitN(s, sep, 2)
@@ -29,13 +47,25 @@ func ParseMessage(l string) *Message {
 		}
 		return parts[0], ""
 	}
-	parsePrefix := func(l string) (head string, tail string) {
+	parsePrefix := func(l string) (prefix *Prefix, tail string) {
+		prefix = new(Prefix)
 		if l == "" || l[0] != ':' {
-			return "", l
+			return prefix, l
 		}
 
-		head, tail = split2(l, " ")
-		return head[1:], tail
+		head, tail := split2(l[1:], " ")
+		user := strings.Index(head, "!")
+		host := strings.Index(head, "@")
+		if host > user {
+			prefix.Host = head[host+1:]
+			head = head[:host]
+		}
+		if user > 0 {
+			prefix.User = head[user+1:]
+			head = head[:user]
+		}
+		prefix.Name = head
+		return prefix, tail
 	}
 	parseCommand := func(l string) (res string, tail string) {
 		return split2(l, " ")
@@ -61,9 +91,7 @@ func ParseMessage(l string) *Message {
 }
 
 // IRC connection struct
-type Connection struct {
-	SSL bool
-
+type Conn struct {
 	rwc    io.ReadWriteCloser
 	reader *bufio.Reader
 
@@ -71,19 +99,34 @@ type Connection struct {
 	wl sync.Mutex
 }
 
+// Create a new IRC connection
+func NewConn(rwc io.ReadWriteCloser) *Conn {
+	return &Conn{
+		reader: bufio.NewReader(rwc),
+		rwc:    rwc,
+	}
+}
+
 // Establish a connection
-func (c *Connection) Dial(addr string) error {
+func Dial(addr string) (*Conn, error) {
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	c.rwc = conn
-	c.reader = bufio.NewReader(c.rwc)
-	return nil
+	return NewConn(conn), nil
+}
+
+//  Establish a secure connection
+func DialTLS(addr string, config *tls.Config) (*Conn, error) {
+	conn, err := tls.Dial("tcp", addr, config)
+	if err != nil {
+		return nil, err
+	}
+	return NewConn(conn), nil
 }
 
 // Reads the next message
-func (c *Connection) ReadMsg() (*Message, error) {
+func (c *Conn) ReadMsg() (*Message, error) {
 	c.rl.Lock()
 	defer c.rl.Unlock()
 	l, err := c.reader.ReadString('\n')
@@ -94,7 +137,7 @@ func (c *Connection) ReadMsg() (*Message, error) {
 }
 
 // Writes Raw bytes
-func (c *Connection) Write(msg []byte) (int, error) {
+func (c *Conn) Write(msg []byte) (int, error) {
 	c.wl.Lock()
 	defer c.wl.Unlock()
 
@@ -102,12 +145,12 @@ func (c *Connection) Write(msg []byte) (int, error) {
 }
 
 // Writes string. "\r\n" will be appended
-func (c *Connection) Send(l string) error {
+func (c *Conn) Send(l string) error {
 	_, err := c.Write([]byte(l + "\r\n"))
 	return err
 }
 
 // Closes connection
-func (c *Connection) Close() error {
+func (c *Conn) Close() error {
 	return c.rwc.Close()
 }
